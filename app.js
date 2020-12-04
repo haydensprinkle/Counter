d7ce0f734ae451f330d9d8096becffc9ad4689fa
haydensprinkle <haydensprinkle@gmail.com>
2020-12-03 21:49:42 -0800

    Added basic comments to highlight code structure

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//variables and selectors
 let count = 0,
   num = document.getElementById("number"),
   btn1 = document.getElementById("increment"),
   btn2 = document.getElementById("decrement");
 
+  //functions
 let increment = () => {
   count += 1;
   num.innerHTML = count;
@@ -11,6 +13,6 @@ let decrement = () => {
   count -= 1;
   num.innerHTML = count;
 };
-
+//event listeners
 btn1.addEventListener("click", increment);
 btn2.addEventListener("click", decrement);
